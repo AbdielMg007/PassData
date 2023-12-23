1591e2f9fe16ee77dc6296e37e7e2f0dc99af083
abdielmg007 <abdiel.mugo@gmail.com>
2023-12-22 21:56:57 -0600

    UPDATE Style

--- passData/SecondViewController.swift
+++ passData/SecondViewController.swift
@@ -5,4 +5,26 @@
 //  Created by Abdiel Mg on 22/12/23.
 //
 
-import Foundation
+import UIKit
+
+protocol SecondViewControllerDelegate {
+    func didTappedButton(message: String)
+}
+
+class SecondViewController: UIViewController {
+    var delegate: SecondViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func passDataButtonTapped(_ sender: Any) {
+        delegate?.didTappedButton(message: "Sent from SecondViewController to FirstViewController")
+    }
+
+}
+
--- passData/ViewController.swift
+++ passData/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SecondViewControllerDelegate {
+    @IBOutlet weak var textViewFromSecondViewController: UITextView!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // find out why you have to prepare for segue!
+        // find ouy why you have to delegate until here
+        // oh! remember the example seen into appRewiewController code
+        if let destination = segue.destination as? SecondViewController {
+            destination.delegate = self
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    @IBAction func performSegueButtonTapped(_ sender: Any) {
+    performSegue(withIdentifier: "goToSecondViewController", sender: nil)
+    }
+    func didTappedButton(message: String) {
+        print(message)
+        let date = Date()
+        print("at \(date)", date)
+        textViewFromSecondViewController.text.append(message + "\(date)")
+        
+    }
 }
-
